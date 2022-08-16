6a0f049f2e21ecd7c4275753ece03e1dd2354d03
MartinHernandez <martinhnz02@gmail.com>
2022-08-16 09:32:39 -0300

    Ejercicio3

--- exercises/exercise3.py
+++ exercises/exercise3.py
@@ -6,12 +6,16 @@ True si 2 personas tienen el mismo nombre pero distinta edad.
 Aclaración: Se puede utilizar and, or y not.
 """
 
+from pickle import TRUE
+
+
 persona_01 = "Kevin"
 edad_01 = 24
 persona_02 = "Kevin"
 edad_02 = 41
 
 # COMPLETAR - INICIO
+comparar_nombre_y_edad= persona_01 = persona_02 and edad_01 != edad_02
 
 # COMPLETAR - FIN
 
@@ -28,7 +32,7 @@ marca_del_auto = "Chevrolet"
 modelo_de_auto = 1998
 
 # COMPLETAR - INICIO
-
+comparar_marca_y_modelo= marca_del_auto != "Ford" and modelo_de_auto<= 2000= TRUE
 # COMPLETAR - FIN
 
 assert comparar_marca_y_modelo
@@ -46,7 +50,7 @@ superficie_de_campo_02 = 851212
 superficie_de_campo_03 = 8512
 
 # COMPLETAR - INICIO
-
+comparar_superficie =
 # COMPLETAR - FIN
 
 assert comparar_superficie
