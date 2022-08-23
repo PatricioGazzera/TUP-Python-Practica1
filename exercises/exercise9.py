e953adca8c4dbf887d2108e61fe4bd53ccc49ccf
Ezequiel Opisacco <ezeopi921@gmail.com>
2022-08-23 11:21:31 -0300

    ejer7

--- exercises/exercise9.py
+++ exercises/exercise9.py
@@ -98,7 +98,7 @@ conjunto_07 = {1, 2, 3, 4, 5, 6, 7, 8}
 conjunto_08 = {2, 4, 6, 8}
 
 # COMPLETAR - INICIO
-conjunto_07 - conjunto_08
+conjunto_diferencia = conjunto_07 - conjunto_08
 # COMPLETAR - FIN
 
 assert conjunto_diferencia == {1, 3, 5, 7}
