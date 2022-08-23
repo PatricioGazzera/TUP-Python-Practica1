5d64e7ab18831f06f9ec43d3d79957ca97985273
Ezequiel Opisacco <ezeopi921@gmail.com>
2022-08-23 11:17:49 -0300

    ejer9

--- exercises/exercise9.py
+++ exercises/exercise9.py
@@ -126,7 +126,7 @@ conjunto_09 = {1, 2, 3, 4, 5, 6, 7, 8, 9}
 conjunto_10 = {1, 2, 3, 5, 6, 7, 8}
 
 # COMPLETAR - INICIO
-conjunto_09 ^ conjunto_10
+conjunto_diferencia_simetrica = conjunto_09 ^ conjunto_10
 # COMPLETAR - FIN
 
 assert conjunto_diferencia_simetrica == {4, 9}
